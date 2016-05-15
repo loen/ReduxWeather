e43515f21878fff86cb980c48c7fd01c6e1c7acb
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-05-15 22:39:06 +0200

    add REST api handling and redux middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_KEY=be70db04674b66158ef3eddcecef0cf8;
+const API_KEY='be70db04674b66158ef3eddcecef0cf8';
 const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export fetchWeather(city) {
-  const url = `ROOT_URL&q=${city},us`;
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  console.log(url);
   const request = axios.get(url);
+
   return {
     type: FETCH_WEATHER,
     payload: request
